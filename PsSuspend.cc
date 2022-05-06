5224e2a3ba2b9062649b354dd8e7f5471f9e8f5a
supericeliu <24412720+mrchaofan@users.noreply.github.com>
2022-05-07 00:06:47 +0800

    Initial commit

---
+++ PsSuspend.cc
@@ -0,0 +1,22 @@
+﻿#include <windows.h>
+
+typedef NTSTATUS(NTAPI* NtSuspendProcess)(IN HANDLE ProcessHandle);
+
+static NtSuspendProcess pfnNtSuspendProcess = (NtSuspendProcess)GetProcAddress(
+	GetModuleHandle(L"ntdll"), "NtSuspendProcess");
+
+static inline NTSTATUS SuspendProcess(DWORD pid)
+{
+	HANDLE processHandle = OpenProcess(PROCESS_SUSPEND_RESUME, FALSE, pid);
+	NTSTATUS status = pfnNtSuspendProcess(processHandle);
+	CloseHandle(processHandle);
+	return status;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2) {
+		DWORD pid = atoi(argv[1]);
+		return SuspendProcess(pid);
+	}
+}
